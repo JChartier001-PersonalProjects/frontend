7c8c85791a5aee6d3b6e4ac32ad8a9228e5739dc
stvsu <ss@stvsu.com>
2020-02-02 14:01:15 -0800

    Add first route and styled Login Form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom'
+
+//components
+import LoginForm from './components/LoginForm'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Droom</h1>
-    </div>
-  );
+    <>
+      <Route path='/login' component={LoginForm} />
+    </>
+  )
 }
 
 export default App;
